96a0c74b717eff455e5e13c3d6590817490298fd
Drake Wheeler <109574097+Copypaster3000@users.noreply.github.com>
2024-09-17 17:43:40 -0700

    edited comments

--- defense_card.h
+++ defense_card.h
@@ -1,8 +1,5 @@
 //defense_card.h
 //Drake Wheeler
-//Karla Fant
-//CS302
-//Program 1
 //6/27/2024
 //This is the header file for the defense_card class which is a derivation of the card class. This class can be used to facilitate
 //defending against an attack card, healing a player with the defense card's heal lvl, and editing defense card stats
