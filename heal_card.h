02df975f8df9b0beff518f33c8b6adc0b9728bb2
Drake Wheeler <109574097+Copypaster3000@users.noreply.github.com>
2024-09-17 17:44:41 -0700

    edited comments

--- heal_card.h
+++ heal_card.h
@@ -1,8 +1,5 @@
 //heal_card.h
 //Drake Wheeler
-//Karla Fant
-//CS302
-//Program 1
 //6/27/2024
 //This is the header file for the heal_card class which is a derivation of the card class. This class manages heal cards,
 //can heal player's health, and allow clients to manage the heal card's strength.
